0e825de3c6460740830e732f201420c8f2740f0a
OTA Daichi <daichi.ota@access-company.com>
2013-07-19 09:03:18 +0900

    Create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Counter::Application.config.secret_token = 'b900ac90d03262b713646607bd338ab923f41f50b0bc581ba46debd8f4965806b8d86b4721558af7029b0ab0572bc205c7304bdd4619bab80ec710ff5fc6eb6c'
